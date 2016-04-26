9f8fad58eb3080e58432e8796ef26dc05b88178b
FranklinMaQ <marcelinaplaneta@interia.pl>
2016-04-26 21:37:20 +0200

    Introduction to Tweaks

---
+++ MaQRepository/src/main/java/maq/repository/view/SwitchBean.java
@@ -0,0 +1,26 @@
+
+package maq.repository.view;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="switchBean")
+@ApplicationScoped
+public class SwitchBean {  
+  
+    private String value = "case1";  
+      
+    public String getValue() {  
+        return value;  
+    }  
+  
+    public void setValue(final String value) {  
+        this.value = value;  
+    }  
+    
+    public void listener(String case_)
+    {
+        System.out.println(case_);
+    }
+} 
